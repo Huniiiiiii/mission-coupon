6c9c1aa10e611afc40e235a88c2c6c8b734d4aba
Huniiiiiii <rtg8464@gmail.com>
2025-03-24 16:44:18 +0900

    [feat] View추가

--- src/main/java/mission/View/View.java
+++ src/main/java/mission/View/View.java
@@ -1,4 +1,21 @@
 package mission.View;
 
+import java.util.Scanner;
+
 public class View {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void printMenu() {
+        System.out.println("쿠폰 적립 시스템입니다. 사용할 기능을 선택해주세요.");
+        System.out.println("1) 회원 등록, 2) 쿠폰 검색, 3) 쿠폰 적립, 4) 쿠폰 사용 5) 프로그램 종료");
+    }
+
+    public String input(String message) {
+        System.out.print(message);
+        return scanner.nextLine();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
 }
