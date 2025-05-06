c06db888de15340355f595028d50447cb6764f39
Huniiiiiii <rtg8464@gmail.com>
2025-05-06 21:56:39 +0900

    [refactor] member 예외 처리 구문 추가

--- src/main/java/mission/Model/Member.java
+++ src/main/java/mission/Model/Member.java
@@ -1,8 +1,8 @@
 package mission.Model;
 
 public class Member {
-    private String name;
-    private String phone;
+    private final String name;
+    private final String phone;
     private int coupons;
 
     public Member(String name, String phone) {
@@ -12,14 +12,19 @@ public class Member {
     }
 
     public String getName() { return name; }
+
     public String getPhone() { return phone; }
+
     public int getCoupons() { return coupons; }
 
     public void addCoupons(int count) {
+        if (count < 0) throw new IllegalArgumentException("쿠폰 적립 수는 0 이상이어야 합니다.");
         this.coupons += count;
     }
 
     public void useCoupons(int count) {
+        if (count < 0) throw new IllegalArgumentException("쿠폰 사용 수는 0 이상이어야 합니다.");
+        if (count > this.coupons) throw new IllegalArgumentException("보유 쿠폰보다 많이 사용할 수 없습니다.");
         this.coupons -= count;
     }
 }
